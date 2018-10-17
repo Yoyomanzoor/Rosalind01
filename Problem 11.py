8241c84e76b8c1ea3fc586e44bffb9802281a300
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-17 10:51:26 -0500

    Problem 11
    Still can't get the answer

--- Problem 11.py
+++ Problem 11.py
@@ -10,27 +10,27 @@ fasta_files = dict()
 
 for i in range(len(f_list)):
     accessId = f_list[i]
-    handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id=accessId)
+    handle = Entrez.efetch(db="nucleotide", rettype="fasta", id=accessId)
     fasta_files[f_list[i]] = SeqIO.read(handle, "fasta")
 
 print(fasta_files)
+
 #with open("Problem 11 answer.txt", "w") as fp:
 #    json.dump(fasta_files, fp)
 
-sequences = dict()
-seqFragments = []
-for line in fasta_files[f_list[0]]:
-    if line.startswith('>'):
-        if seqFragments:
-            sequence = ''.join(seqFragments)
-            sequences[id] = sequence
-        seqFragments = []
-        id = line.rstrip()[1:]
-    else:
-        seq = line.rstrip()
-        seqFragments.append(seq)
-if seqFragments:
-    sequence = ''.join(seqFragments)
-    sequences[id] = sequence
-
-print(sequence)
+# sequences = dict()
+# seqFragments = []
+# for line in fasta_files[f_list[0]]:
+#     if line.startswith('>'):
+#         if seqFragments:
+#             sequence = ''.join(seqFragments)
+#             sequences[id] = sequence
+#         seqFragments = []
+#         id = line.rstrip()[1:]
+#     else:
+#         seq = line.rstrip()
+#         seqFragments.append(seq)
+# if seqFragments:
+#     sequence = ''.join(seqFragments)
+#     sequences[id] = sequence
+# print(sequence)
