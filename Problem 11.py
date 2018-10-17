8a8ec94ce37aa9c0b9231fe3d6643092c2de2029
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-17 11:03:39 -0500

    Problem 11
    Solved!

--- Problem 11.py
+++ Problem 11.py
@@ -1,4 +1,3 @@
-import json
 from Bio import SeqIO, Entrez
 Entrez.email = "smanzoor@luc.edu"
 
@@ -13,23 +12,3 @@ for i in range(len(f_list)):
     fasta_files[handle.read()] = f_list[i]
 
 f_answer = open("Problem 11 answer.txt", "w").write(min(fasta_files, key=len))
-
-#with open("Problem 11 answer.txt", "w") as fp:
-#    json.dump(fasta_files, fp)
-
-# sequences = dict()
-# seqFragments = []
-# for line in fasta_files[f_list[0]]:
-#     if line.startswith('>'):
-#         if seqFragments:
-#             sequence = ''.join(seqFragments)
-#             sequences[id] = sequence
-#         seqFragments = []
-#         id = line.rstrip()[1:]
-#     else:
-#         seq = line.rstrip()
-#         seqFragments.append(seq)
-# if seqFragments:
-#     sequence = ''.join(seqFragments)
-#     sequences[id] = sequence
-# print(sequence)
