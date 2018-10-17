185c048b1564c5c41f02b4232749aee34ccba235
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-16 22:27:27 -0500

    Problem 10 answer
    Haven't tried actual dataset yet

--- Problem 10.py
+++ Problem 10.py
@@ -1,2 +1,11 @@
-f_makefile = open("Problem 10.txt", "w")
-f_makeanotha = open("Problem 10 answer.txt", "w")
+f_dataset = open("Problem 10.txt", "r").read()
+s = f_dataset.split("\n")
+s1 = s[0]
+s2 = s[1]
+count = 0
+
+for i in range(len(s1)):
+    if s1[i] != s2[i]:
+        count += 1
+
+f_answer = open("Problem 10 answer.txt", "w").write(str(count))
