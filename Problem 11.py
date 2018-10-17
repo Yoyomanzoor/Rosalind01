27de36035013cea5549d1c9590d251db84e355b7
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-16 23:50:22 -0500

    Problem 11 again

--- Problem 11.py
+++ Problem 11.py
@@ -1,2 +1,36 @@
-open("Problem 11.txt", "w")
-open("Problem 11 answer.txt", "w")
+import json
+from Bio import SeqIO, Entrez
+Entrez.email = "smanzoor@luc.edu"
+
+f_dataset = open("Problem 11.txt", "r").read()
+f_list = f_dataset.split(" ")
+print(f_list)
+accessId = ""
+fasta_files = dict()
+
+for i in range(len(f_list)):
+    accessId = f_list[i]
+    handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id=accessId)
+    fasta_files[f_list[i]] = SeqIO.read(handle, "fasta")
+
+print(fasta_files)
+#with open("Problem 11 answer.txt", "w") as fp:
+#    json.dump(fasta_files, fp)
+
+sequences = dict()
+seqFragments = []
+for line in fasta_files[f_list[0]]:
+    if line.startswith('>'):
+        if seqFragments:
+            sequence = ''.join(seqFragments)
+            sequences[id] = sequence
+        seqFragments = []
+        id = line.rstrip()[1:]
+    else:
+        seq = line.rstrip()
+        seqFragments.append(seq)
+if seqFragments:
+    sequence = ''.join(seqFragments)
+    sequences[id] = sequence
+
+print(sequence)
