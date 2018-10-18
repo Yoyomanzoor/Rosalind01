6908679b5b5af85c4f6859356c52f00c31de493a
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-18 01:35:05 -0500

    Problem 12

--- Problem 12.py
+++ Problem 12.py
@@ -1,2 +1,23 @@
-open("Problem 12.txt", "w")
-open("Problem 12 answer.txt", "w")
+from Bio import ExPASy
+from Bio import SwissProt
+
+f_ID = open("Problem 12.txt", "r").read()
+
+handle = ExPASy.get_sprot_raw(f_ID)
+record = SwissProt.read(handle)
+
+GO_Bio_Processes = []
+temp_list = []
+
+for item in record.cross_references:
+    if item[0] == 'GO':
+        temp_list = item[2].split(":")
+        if temp_list[0] == 'P':
+            GO_Bio_Processes.append(temp_list[1])
+
+f_answer = ""
+
+for i in range(len(GO_Bio_Processes)):
+    f_answer += GO_Bio_Processes[i] + "\n"
+
+open("Problem 12 answer.txt", "w").write(f_answer)
