740774d8ef5ac60e951cc9a57b88352b4ec4db29
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-18 19:21:03 -0500

    Cleaned up Problem 13

--- Problem 13.py
+++ Problem 13.py
@@ -2,90 +2,62 @@ f_dataset = open("Problem 13.txt", "r").read()
 
 codon_dictionary = {"TTT": "F", "TTC": "F", "TTA": "L", "TTG": "L", "TCT": "S", "TCC": "S", "TCA": "S", "TCG": "S", "TAT": "Y", "TAC": "Y", "TAA": "Stop", "TAG": "Stop", "TGT": "C", "TGC": "C", "TGA": "Stop", "TGG": "W", "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L", "CCT": "P", "CCC": "P", "CCA": "P", "CCG": "P", "CAT": "H", "CAC": "H", "CAA": "Q", "CAG": "Q", "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R", "ATT": "I", "ATC": "I", "ATA": "I", "ATG": "M", "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T", "AAT": "N", "AAC": "N", "AAA": "K", "AAG": "K", "AGT": "S", "AGC": "S", "AGA": "R", "AGG": "R", "GTT": "V", "GTC": "V", "GTA": "V", "GTG": "V", "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A", "GAT": "D", "GAC": "D", "GAA": "E", "GAG": "E", "GGT": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
 
-def complement_strand(sequence):
-    reverseStrand = ""
+def find_seq(dataset):
+    seq_from_data = ""
+    f_list = f_dataset.split("\n")
+    for i in range(len(f_list)):
+        if f_list[i][0] == "A" or f_list[i][0] == "C" or f_list[i][0] == "G" or f_list[i][0] == "T":
+            seq_from_data = f_list[i]
+
+    return seq_from_data
+
+def find_complement_strand(sequence):
+    complement_strand = ""
     for i in range(len(sequence)):
         if sequence[i] == "A":
-            reverseStrand += "T"
+            complement_strand += "T"
         elif sequence[i] == "T":
-            reverseStrand += "A"
+            complement_strand += "A"
         elif sequence[i] == "C":
-            reverseStrand += "G"
+            complement_strand += "G"
         elif sequence[i] == "G":
-            reverseStrand += "C"
-    return reverseStrand
+            complement_strand += "C"
 
-def find_ORFs(sequence):
+    return complement_strand
+
+def reverse_seq_order(sequence):
+    reverse_sequence = ""
+    for i in range(len(sequence)):
+        reverse_sequence += sequence[len(sequence_complement) - (i + 1)]
+
+    return reverse_sequence
+
+def find_ORFs(seq):
     ORF1 = ""
     ORF2 = ""
     ORF3 = ""
 
-    for i in range(int(len(sequence_reverse_complement) / 3)):
+    for i in range(int(len(seq) / 3)):
         j = i
         placement = (j * 3)
-        if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-            ORF1 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
+        if seq[placement:placement + 3] in codon_dictionary:
+            ORF1 += codon_dictionary[seq[placement:placement + 3]]
         placement = ((j * 3) + 1)
-        if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-            ORF2 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
+        if seq[placement:placement + 3] in codon_dictionary:
+            ORF2 += codon_dictionary[seq[placement:placement + 3]]
         placement = ((j * 3) + 2)
-        if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-            ORF3 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
+        if seq[placement:placement + 3] in codon_dictionary:
+            ORF3 += codon_dictionary[seq[placement:placement + 3]]
+
     return ORF1, ORF2, ORF3
 
-sequence = ""
-f_list = f_dataset.split("\n")
+sequence = find_seq(f_dataset)
 
-for i in range(len(f_list)):
-    if f_list[i][0] == "A" or f_list[i][0] == "C" or f_list[i][0] == "G" or f_list[i][0] == "T":
-        sequence = f_list[i]
+sequence_complement = find_complement_strand(sequence)
 
+sequence_reverse_complement = reverse_seq_order(sequence_complement)
 
-
-current_read = ""
-
-ORF1 = ""
-ORF2 = ""
-ORF3 = ""
-
-for i in range(int(len(sequence)/3)):
-    j = i
-    placement = (j*3)
-    if sequence[placement:placement+3] in codon_dictionary:
-        ORF1 += codon_dictionary[sequence[placement:placement + 3]]
-    placement = ((j*3)+1)
-    if sequence[placement:placement+3] in codon_dictionary:
-        ORF2 += codon_dictionary[sequence[placement:placement + 3]]
-    placement = ((j*3)+2)
-    if sequence[placement:placement + 3] in codon_dictionary:
-        ORF3 += codon_dictionary[sequence[placement:placement + 3]]
-
-sequence_complement = complement_strand(sequence)
-
-sequence_reverse_complement = ""
-for i in range(len(sequence_complement)):
-    sequence_reverse_complement += sequence_complement[len(sequence_complement) - (i + 1)]
-
-ORF4 = ""
-ORF5 = ""
-ORF6 = ""
-
-for i in range(int(len(sequence_reverse_complement) / 3)):
-    j = i
-    placement = (j*3)
-    if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-        ORF4 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
-    placement = ((j*3)+1)
-    if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-        ORF5 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
-    placement = ((j*3)+2)
-    if sequence_reverse_complement[placement:placement + 3] in codon_dictionary:
-        ORF6 += codon_dictionary[sequence_reverse_complement[placement:placement + 3]]
-
-print(ORF1)
-print(ORF2)
-print(ORF3)
-print(ORF4)
-print(ORF5)
-print(ORF6)
-
+ORF_set1 = find_ORFs(sequence)
+print(ORF_set1)
+ORF_set2 = find_ORFs(sequence_reverse_complement)
+print(ORF_set2)
