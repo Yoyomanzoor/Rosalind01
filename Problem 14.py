a29c8f2382a046655e969372ba16a563b66d0c77
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-22 01:30:13 -0500

    Fixed P14 typo

--- Problem 14.py
+++ Problem 14.py
@@ -1,16 +1,12 @@
 from Bio import ExPASy, SwissProt
-import time
 
 def find_multiple_sequences_from_UniProtIDs(ID_list):
     sequences = ""
     i = 0
     for item in ID_list:
-        try:
-            handle = ExPASy.get_sprot_raw(ID_list[i])
-            record = SwissProt.read(handle)
-            sequences += record.sequence + "\n"
-        except TimeoutError:
-            print("Could not retrieve sequences")
+        handle = ExPASy.get_sprot_raw(ID_list[i])
+        record = SwissProt.read(handle)
+        sequences += record.sequence + "\n"
         i += 1
     return sequences
 
@@ -35,6 +31,7 @@ def make_outfile(accession_IDs_list, ID_markers_list, motif_locations_list):
     outfile = ""
     for n in range(len(ID_markers_list)):
         outfile += accession_IDs_list[ID_markers_list[n]] + "\n" + motif_locations_list[n] + "\n"
+    print(motif_locations_list)
     return outfile
 
 
@@ -52,6 +49,6 @@ NGlyc_motif_locations, ID_marker = find_NGlyc_motifs(seq_col_list)
 
 NGlyc_locations_list = NGlyc_motif_locations.split("\n")
 
-outfile = make_outfile(f_IDs, ID_marker, NGlyc_motif_locations)
+outfile = make_outfile(f_IDs, ID_marker, NGlyc_locations_list)
 
 open("Problem 14 answer.txt", "w").write(outfile)
