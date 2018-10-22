5d94cfadaf3386d64a6f9eef0297a0de84511ddd
Yoyo Manzoor <smanzoor@luc.edu>
2018-10-22 01:14:31 -0500

    Problem 14 cleaned, getting HTTP errors
    Think it's due to subcategories/urls not being specific enough

--- Problem 14.py
+++ Problem 14.py
@@ -1,4 +1,42 @@
 from Bio import ExPASy, SwissProt
+import time
+
+def find_multiple_sequences_from_UniProtIDs(ID_list):
+    sequences = ""
+    i = 0
+    for item in ID_list:
+        try:
+            handle = ExPASy.get_sprot_raw(ID_list[i])
+            record = SwissProt.read(handle)
+            sequences += record.sequence + "\n"
+        except TimeoutError:
+            print("Could not retrieve sequences")
+        i += 1
+    return sequences
+
+
+def find_NGlyc_motifs(sequence_list):
+    NGlyc_motif_locations = ""
+    ID_marker = []
+    NGlyc_motif_counter = 0
+    for j in range(len(sequence_list)):
+        for m in range(len(sequence_list[j])):
+            if sequence_list[j][m] == "N" and sequence_list[j][m + 1] != "P" and (sequence_list[j][m + 2] == "S" or sequence_list[j][m + 2] == "T") and sequence_list[j][m + 3] != "P":
+                NGlyc_motif_locations += str(m + 1) + " "
+                NGlyc_motif_counter += 1
+        if NGlyc_motif_counter > 0:
+            NGlyc_motif_locations += "\n"
+            ID_marker.append(j)
+        NGlyc_motif_counter = 0
+    return NGlyc_motif_locations, ID_marker
+
+
+def make_outfile(accession_IDs_list, ID_markers_list, motif_locations_list):
+    outfile = ""
+    for n in range(len(ID_markers_list)):
+        outfile += accession_IDs_list[ID_markers_list[n]] + "\n" + motif_locations_list[n] + "\n"
+    return outfile
+
 
 f_dataset = open("Problem 14.txt").read()
 
@@ -6,33 +44,14 @@ f_IDs = [f_dataset]
 if "\n" in f_dataset:
     f_IDs = f_dataset.split("\n")
 
-sequence_collection = ""
-i = 0
-for item in f_IDs:
-    handle = ExPASy.get_sprot_raw(f_IDs[i])
-    record = SwissProt.read(handle)
-    sequence_collection += record.sequence + "\n"
-    i += 1
+sequence_collection = find_multiple_sequences_from_UniProtIDs(f_IDs)
 
 seq_col_list = sequence_collection.split("\n")
 
-NGlyc_motif_locations = ""
-ID_marker = []
-NGlyc_motif_counter = 0
-for j in range(len(seq_col_list)):
-    for m in range(len(seq_col_list[j])):
-        if seq_col_list[j][m] == "N" and seq_col_list[j][m+1] != "P" and (seq_col_list[j][m+2] == "S" or seq_col_list[j][m+2] == "T") and seq_col_list[j][m+3] != "P":
-            NGlyc_motif_locations += str(m + 1) + " "
-            NGlyc_motif_counter += 1
-    if NGlyc_motif_counter > 0:
-        NGlyc_motif_locations += "\n"
-        ID_marker.append(j)
-    NGlyc_motif_counter = 0
+NGlyc_motif_locations, ID_marker = find_NGlyc_motifs(seq_col_list)
 
 NGlyc_locations_list = NGlyc_motif_locations.split("\n")
 
-outfile = ""
-for n in range(len(ID_marker)):
-    outfile += f_IDs[ID_marker[n]] + "\n" + NGlyc_locations_list[n] + "\n"
+outfile = make_outfile(f_IDs, ID_marker, NGlyc_motif_locations)
 
-print(outfile)
+open("Problem 14 answer.txt", "w").write(outfile)
